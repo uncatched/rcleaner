39238b7c6a03027c59dc6f9cbf3653cfa5fdf18e
Denys Litvinskyi <litvinskyi.dev@gmail.com>
2024-07-23 16:38:56 +0300

    Remove files

--- target/debug/deps/equivalent-42b0b1671c92fc21.d
+++
@@ -1,5 +0,0 @@
-/Users/denyslitvinskyi/Documents/Projects/Personal/rcleaner/target/debug/deps/libequivalent-42b0b1671c92fc21.rmeta: /Users/denyslitvinskyi/.cargo/registry/src/index.crates.io-6f17d22bba15001f/equivalent-1.0.1/src/lib.rs
-
-/Users/denyslitvinskyi/Documents/Projects/Personal/rcleaner/target/debug/deps/equivalent-42b0b1671c92fc21.d: /Users/denyslitvinskyi/.cargo/registry/src/index.crates.io-6f17d22bba15001f/equivalent-1.0.1/src/lib.rs
-
-/Users/denyslitvinskyi/.cargo/registry/src/index.crates.io-6f17d22bba15001f/equivalent-1.0.1/src/lib.rs:
--- target/debug/deps/itoa-8df19e56961026d1.d
+++
@@ -1,6 +0,0 @@
-/Users/denyslitvinskyi/Documents/Projects/Personal/rcleaner/target/debug/deps/libitoa-8df19e56961026d1.rmeta: /Users/denyslitvinskyi/.cargo/registry/src/index.crates.io-6f17d22bba15001f/itoa-1.0.11/src/lib.rs /Users/denyslitvinskyi/.cargo/registry/src/index.crates.io-6f17d22bba15001f/itoa-1.0.11/src/udiv128.rs
-
-/Users/denyslitvinskyi/Documents/Projects/Personal/rcleaner/target/debug/deps/itoa-8df19e56961026d1.d: /Users/denyslitvinskyi/.cargo/registry/src/index.crates.io-6f17d22bba15001f/itoa-1.0.11/src/lib.rs /Users/denyslitvinskyi/.cargo/registry/src/index.crates.io-6f17d22bba15001f/itoa-1.0.11/src/udiv128.rs
-
-/Users/denyslitvinskyi/.cargo/registry/src/index.crates.io-6f17d22bba15001f/itoa-1.0.11/src/lib.rs:
-/Users/denyslitvinskyi/.cargo/registry/src/index.crates.io-6f17d22bba15001f/itoa-1.0.11/src/udiv128.rs:
